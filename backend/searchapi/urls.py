eb02f57511b5ca98c182ce6f7d1394493015c27f
AkshayKencharaddi <akshayrameshkencharaddi@gmail.com>
2025-04-05 00:56:16 +0530

    Add files via upload

---
+++ backend/searchapi/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import search,RegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('search/', search, name='search'),
+    path('register/',RegisterView.as_view(),name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
